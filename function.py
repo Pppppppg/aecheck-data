d362afa8c0abc9f95b3b0ddad3f5c45766ef4ce2
Hyunuk Lee <as0266@naver.com>
2022-09-22 12:04:26 +0900

    20220922 JAP 2.14.30, GLO 2.13.500

--- function.py
+++ function.py
@@ -30,6 +30,6 @@ def add_data_to_sheet(tag: str, sheet, data_list):
     sheet_list = [i[0].value for i in sheet.iter_rows(min_row=2)]
     
     for key in list(set(data_list)):
-        if key not in sheet_list:
+        if key is not None and key not in sheet_list:
             print("새로 등록, 번역 확인 필요 : {} - {}".format(tag, key))
             sheet.append([key])
